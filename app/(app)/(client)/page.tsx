e4a82ae0917c1027233a7548fa042e19d999225c
admin12121 <vickytaj6459@gmail.com>
2026-04-22 01:15:53 +0545

    feat: add hero and what-we-do sections styles to index.css

--- app/(app)/(client)/page.tsx
+++ app/(app)/(client)/page.tsx
@@ -888,7 +888,6 @@ export default function Page() {
       <div className="relative w-screen overflow-x-hidden">
         <HeroSection heroImageRef={heroImageRef} />
         <AboutDescriptionSection />
-        {/* <FeaturedWorkSection onInternalLinkClick={handleInternalLinkClick} /> */}
         <ServicesHeaderSection />
         <ServicesStackSection />
         <ContactCtaSection />
